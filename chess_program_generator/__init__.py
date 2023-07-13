8e5b2b7013e0c6e0faefb37ef852252957bb7671
Miguel Guthridge <hdsq@outlook.com.au>
2023-07-13 21:08:03 +1000

    Expose CLI in Poetry config

--- chess_program_generator/__init__.py
+++ chess_program_generator/__init__.py
@@ -5,6 +5,8 @@ A tool for generating chess programs
 """
 __version__ = '1.0.0'
 __all__ = [
-    'generate'
+    'generate',
+    'main',
 ]
 from .generator import generate
+from .__main__ import main
--- chess_program_generator/__main__.py
+++ chess_program_generator/__main__.py
@@ -2,16 +2,19 @@ from argparse import ArgumentParser
 from .generator import generate
 
 
-parser = ArgumentParser(
-    'chess_program_generator',
-    description='Generate a Python program for playing chess',
-)
+def main() -> None:
+    parser = ArgumentParser(
+        'chess_program_generator',
+        description='Generate a Python program for playing chess',
+    )
+
+    parser.add_argument('depth', action='store', type=int)
+
+    args = parser.parse_args()
+
+    depth: int = args.depth
+    generate(depth)
 
 
-parser.add_argument('depth', action='store', type=int)
-
-
-args = parser.parse_args()
-
-depth: int = args.depth
-generate(depth)
+if __name__ == '__main__':
+    main()
